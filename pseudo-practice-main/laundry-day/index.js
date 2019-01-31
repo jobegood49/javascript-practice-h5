0d3541c3beba45fbce2739a4353fe9e33084784b
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-31 12:11:43 +0700

    Improve laundry day code quality

--- pseudo-practice-main/laundry-day/index.js
+++ pseudo-practice-main/laundry-day/index.js
@@ -1,17 +1,40 @@
 /*
-STORE "number of clothes" with value of 20
-STORE "current number" with value of 0
+GET "number of clothes" from input
+SET "current total" with 0
 WHILE
   ("current number" is smaller or equal to 20)
-  INCREMENT the "current number by 1"
-ENDWHILE
+  INCREMENT the "current total by 1"
+END-WHILE
 DISPLAY "The washing machine is ready to start"
 */
 
-let numberOfClothes = 20
-let currentNumber = 0
-while (currentNumber <= 20) {
-  console.log(`${currentNumber} is the number of clothes inside the machine`)
-  currentNumber++
+// let numberOfClothes = 20
+// let currentNumber = 0
+
+// while (currentNumber <= 20) {
+//   console.log(`${currentNumber} is the number of clothes inside the machine`)
+//   currentNumber++
+// }
+// console.log('the machine is ready to start')
+
+const numberOfClothes = prompt('Number of clothes?')
+let currentTotal = 0
+
+while (currentTotal <= numberOfClothes) {
+  console.log(`${currentTotal} is the number of clothes inside the machine`)
+  currentTotal++
 }
-console.log('the machine is ready to start')
+
+console.log('The machine is ready to start')
+
+// Future example
+const clothes = [
+  {
+    type: 'Shirt',
+    color: 'Blue'
+  },
+  {
+    type: 'Pants',
+    color: 'Black'
+  }
+]
